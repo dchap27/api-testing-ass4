fe9db9758978aa90a8e43d50811c3ad1664163fb
Ahmad Adelaja <ahmadadelaja@gmail.com>
2016-05-20 18:32:08 +0100

    add the jquery function to activate the response

--- apitesting-ass.js
+++ apitesting-ass.js
@@ -7,11 +7,23 @@ function jokesApplication(value){
 		var xhr = new XMLHttpRequest();
 		xhr.open("GET", source + value, false);
 		xhr.send();
-		console.log(xhr.response);
+		//console.log(xhr.response);
+		var resp = JSON.parse(xhr.response);
+		console.log(resp.value[0].joke)
+		return resp.value[0].joke;
 
         //return (this.notes.push(this.content));// adds new content to the list of notes
 		};
 }
 
-var jokesapp = new jokesApplication(prompt('input the number of jokes to display'));
-jokesapp.getJoke();
+
+
+$(document).ready(function() {
+
+
+  $('.joke-button').click(function() {
+  	var jokesapp = new jokesApplication(1);
+	
+    $('.new-jokes').html(jokesapp.getJoke());
+  });
+});
